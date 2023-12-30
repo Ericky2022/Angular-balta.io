efceffb7f7f0864d8bd240cf6ebb3fc64f7f6ece
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2023-12-29 22:05:49 -0300

    Lendo itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class AppComponent {
         Validators.required
       ])]
     });
+    this.load();
   }
 
   add(){
@@ -43,18 +44,26 @@ export class AppComponent {
     if(index != -1){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
 
   markUnDone(todo: |Todo){
     todo.done = false;
+    this.save();
   }
 
   save(){
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
   }
 }
